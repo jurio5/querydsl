790594054e532f0ed67811b00da21e953b3cb47f
jurio5 <sia5492@naver.com>
2025-07-16 00:05:05 +0900

    [feat] QueryDSL 검색 조건 쿼리

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,96 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory query;
+
+    @BeforeEach
+    void before() {
+        query = new JPAQueryFactory(em);
+
+        Team teamA = Team.of("teamA");
+        Team teamB = Team.of("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = createMember("member1", 10, teamA);
+        Member member2 = createMember("member2", 20, teamA);
+        Member member3 = createMember("member3", 30, teamB);
+        Member member4 = createMember("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    void startJPQL() {
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void startQuerydsl() {
+        Member findMember = query
+                .selectFrom(member)
+                .where(member.username.eq("member1")) // 파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    void search() {
+        Member findMember = query
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.goe(10))
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void searchAndParam() {
+        Member findMember = query
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    private Member createMember(String username, int age, Team teamA) {
+        return Member.builder()
+                .username(username)
+                .age(age)
+                .team(teamA)
+                .build();
+    }
+}
