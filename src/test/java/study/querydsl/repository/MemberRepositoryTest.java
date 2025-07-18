cb9c5b075890635b597ab71e5cf4f154b29fb3ab
jurio5 <sia5492@naver.com>
2025-07-19 01:43:11 +0900

    [feat] Spring Data JPA 커스텀 리포지터리 페이징 방식 및 fetchResult , fetch & count 쿼리 분리 (후자가 베스트 프랙티스(지연 쿼리 최적화 방식))

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -67,6 +69,31 @@ class MemberRepositoryTest {
 
 //        assertThat(result).extracting("username").containsExactly("member3","member4");
         assertThat(result).extracting("username").containsExactly("member4");
+    }
+
+ @Test
+    void searchPageSimple() {
+        Team teamA = Team.of("teamA");
+        Team teamB = Team.of("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = createMember("member1", 10, teamA);
+        Member member2 = createMember("member2", 20, teamA);
+        Member member3 = createMember("member3", 30, teamB);
+        Member member4 = createMember("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+     Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
+
+     assertThat(result.getSize()).isEqualTo(3);
+     assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
 
     private Member createMember(String username, int age, Team teamA) {
