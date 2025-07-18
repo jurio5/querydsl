2b701ff3b586f2966b780eabd0f5154608504ee9
jurio5 <sia5492@naver.com>
2025-07-18 23:03:34 +0900

    [feat] JPA -> Spring Data JPA 변환 및 테스트 코드

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package study.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 쿼리 메서드 방식으로 동작
+    List<Member> findByUsername(String username);
+}
