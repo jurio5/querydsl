a3204cb837f178ec978aabace29d099c4a23095c
jurio5 <sia5492@naver.com>
2025-07-19 23:43:42 +0900

    [feat] QuerydslPredicateExecutor (명시적 조인이 불가능하고, 묵시적 조인만 가능하여 가급적 사용 X)

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -15,7 +15,7 @@ import study.querydsl.entity.Team;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -94,6 +94,29 @@ class MemberRepositoryTest {
 
      assertThat(result.getSize()).isEqualTo(3);
      assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = Team.of("teamA");
+        Team teamB = Team.of("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = createMember("member1", 10, teamA);
+        Member member2 = createMember("member2", 20, teamA);
+        Member member3 = createMember("member3", 30, teamB);
+        Member member4 = createMember("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+
+        for (Member findMember : result) {
+            System.out.println("member1 = " + findMember);
+        }
     }
 
     private Member createMember(String username, int age, Team teamA) {
